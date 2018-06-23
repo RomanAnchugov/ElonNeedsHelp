9565f29ea9c50ff8812a346351af9a5f1e8415e9
RomanAnchugov <romagn@mail.ru>
2018-06-23 17:44:25 +0300

    random scale

--- main/game/plannet_bg.go
+++ main/game/plannet_bg.go
@@ -17,7 +17,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/main/main.atlas\"\n"
-  "default_animation: \"icon_180\"\n"
+  "default_animation: \"star_blue_giant01\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
