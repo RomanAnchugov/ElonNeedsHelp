4f4b0ba931226d11c68a86ba71c1daf9a1abb23a
RomanAnchugov <romagn@mail.ru>
2018-07-08 10:40:02 +0300

    bonus gui, money per click bonus implementation

--- main/varibles.lua
+++ main/varibles.lua
@@ -1,2 +1,30 @@
 MONEY = 0
 BEST_SCORE = 0
+
+MONEY_PER_CLICK_COUNT = 0
+SECOND_LIFE_COUNT = 0
+ROCKETS_COUNT = 0
+ENGINES_COUNT = 0
+BOOST_COUNT = 0
+AUTOPILOT_COUNT = 0
+
+MONEY_PER_CLICK_COST = 100
+SECOND_LIFE_COST = 100
+ROCKETS_COST = 100
+ENGINES_COST = 100
+BOOOST_COST = 100
+AUTOPILOT_COST = 100
+
+local money_per_click_count = 0
+local secodn_life_count = 0
+local rockets_count = 0
+local engines_count = 0
+local boost_count = 0
+local autopilot_count = 0
+
+local money_per_click_cost = 100
+local second_life_cost = 100
+local rockets_cost = 100
+local engines_cost = 100
+local boost_cost = 100
+local autopilot_cost = 100
