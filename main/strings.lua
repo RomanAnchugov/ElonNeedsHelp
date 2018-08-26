cd0098e88ecb74f209a2407bc15e13728b3243a2
RomanAnchugov <romagn@mail.ru>
2018-08-27 00:28:36 +0300

    localisation

---
+++ main/strings.lua
@@ -0,0 +1,103 @@
+menu_play_btn = {
+	["ru"] = "ИГРАТЬ",
+	["en"] = "PLAY"
+}
+menu_shop_btn = {
+	["ru"] = "МАГАЗ",
+	["en"] = "SHOP"
+}
+
+bonus_money_per_click = {
+	["ru"] = "ДЕНЬГИ ЗА КЛИК",
+	["en"] = "MONEY PER CLICK"
+}
+
+bonus_points_per_click = {
+	["ru"] = "ОЧКИ ЗА КЛИК",
+	["en"] = "POINTS PER CLICK"
+}
+
+bonus_second_life = {
+	["ru"] = "ВТОРАЯ ЖИЗНЬ",
+	["en"] = "SECOND LIFE"
+}
+
+bonus_boost = {
+	["ru"] = "УСКОРЕНИЕ",
+	["en"] = "BOOSTS"
+}
+
+bonus_autopilot = {
+	["ru"] = "АВТОПИЛОТ",
+	["en"] = "AUTOPILOT"
+}
+
+style_engines = {
+	["ru"] = "ДВИГАТЕЛИ",
+	["en"] = "ENGINES"
+}
+
+style_rockets = {
+	["ru"] = "РАКЕТЫ",
+	["en"] = "ROCKETS"
+}
+
+money_text = {
+	["ru"] = "деньги",
+	["en"] = "money"
+}
+
+best_text = {
+	["ru"] = "лучший",
+	["en"] = "best"
+}
+
+back_text = {
+	["ru"] = "НАЗАД",
+	["en"] = "BACK"
+}
+
+power_ups_text = {
+	["ru"] = "УЛУЧШЕНИЯ",
+	["en"] = "POWER UPS"
+}
+
+style_text = {
+	["ru"] = "СТИЛЬ",
+	["en"] = "STYLE"
+}
+
+bonus_text = {
+	["ru"] = "БОНУС",
+	["en"] = "BONUS"
+}
+
+pause_text = {
+	["ru"] = "ПАУЗА",
+	["en"] = "PAUSE"
+}
+
+second_life_text = {
+	["ru"] = "ВТОРАЯ ЖИЗНЬ?",
+	["en"] = "SECOND LIFE?"
+}
+
+no_text = {
+	["ru"] = "НЕ",
+	["en"] = "NO"
+}
+
+yes_text = {
+	["ru"] = "ДА",
+	["en"] = "YES"
+}
+
+sound_text = {
+	["ru"] = "ЗВУК",
+	["en"] = "SOUND"
+}
+
+vibration_text = {
+	["ru"] = "ВИБАРАЦИЯ",
+	["en"] = "VIBRATION"
+}
